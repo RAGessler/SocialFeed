c9acebd111f929d32acca7d3a406fed5aa0c6cf7
Robert Gessler <ragessler@Roberts-MBP.attlocal.net>
2022-05-06 23:26:49 -0500

    changed some styling on input fields

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -24,14 +24,13 @@ const CreatePost = (props) => {
     return(
         <div className='container'>
                 <form onSubmit={handleSubmit} className=''>
-                    <div className='form-group'>
-                        <label htmlFor='postName' >Name</label>
-                        <input id='postName' className='form-control' type="text" value={name} onChange={(event) => setName(event.target.value)}/>
+                    <div className='input-group mb-3' style={{}}>
+                        <span className='input-group-text'>Name</span>
+                        <input id='postName' style={{maxWidth: '40%'}} className='form-control' type="text" value={name} onChange={(event) => setName(event.target.value)}/>
                     </div>
                     <div className='form-group'>
-                        <label htmlFor="postBody">Body</label>
                         <textarea id='postBody' className='form-control form-control-lg' placeholder='Update your status...' aria-label='Body' type="text" value={body} onChange={(event) => setBody(event.target.value)}/>
-                        <button className='btn btn-primary' style={{margin: "0 auto", display: "block", width: "80%"}} type='submit'>Post
+                        <button className='btn btn-primary' style={{margin: "0 auto", marginTop: '10px' , display: "block", width: "50%",}} type='submit'>Post
                         <span className='badge bg-light' style={{color:'black', marginLeft: '1em'}} >{fullDate}</span>
                         </button>
                     </div>
